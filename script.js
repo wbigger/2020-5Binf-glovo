d2babf1010d846309d4f99e0ebee5015647d9370
Claudio Capobianco <claudio.capobianco@marconicloud.it>
2020-12-20 07:39:37 +0000

    Add loop for pizzas

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ var init = function() {
   // tutti gli elementi della pagina con quel tag
   $("title").text("MyGlovo");
 
+  let p = {
+    img_url: 'margherita.jpg',
+    desc: 'Margherita',
+    categ: ["Vegetariana"],
+    price: 7.00
+  }
+
+  let pizzas = [
+    p
+  ]
+for (pizza of pizzas) {
   // Creo un _nuovo_ elemento, con tag div e classe menu_card
   // Nota: quando uso le parentesi angolari creo un nuovo elemento
   let menu_card = $("<div>").addClass("menu_card");
-  let menu_img = $("<img>").attr('src', 'margherita.jpg');
-  let menu_h3 = $("<h3>").text("Margherita");
-  let menu_span = $("<span>").text("Vegetariana");
+  let menu_img = $("<img>").attr('src', pizza.img_url);
+  let menu_h3 = $("<h3>").text(pizza.desc);
+  let menu_span = $("<span>").text(pizza.categ);
   let menu_categories = $("<div>").addClass("menu_categories");
   let menu_details = $("<div>").addClass("menu_details");
-  let menu_price = $("<span>").addClass("menu_price").text("€7.00");
+  let menu_price = $("<span>").addClass("menu_price").text("€"+pizza.price.toFixed(2));
   let menu_add = $("<button>").addClass("menu_add").text("+");
 
   menu_card
@@ -30,6 +41,7 @@ var init = function() {
     );
 
   $(".flexcontainer").append(menu_card);
+}
 
 }
 
